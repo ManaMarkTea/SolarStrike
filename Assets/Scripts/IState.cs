5a0b2a18cddcb3953f935cdaf98d7d72195e4d83
mtulew1 <mtulew1@uic.edu>
2014-02-14 13:47:13 -0600

    Checked in files incorrectly.

--- Assets/Scripts/IState.cs
+++ Assets/Scripts/IState.cs
@@ -3,31 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-
-public abstract class IState : MonoBehaviour
-{	
-	public GameObject StateGameObject;
-	public IState NextState;
-	public IState FutureState;
-	public Stack<IState> ActionHistory;
-	
-	public void Start() {
-		this.NextState = this;
-		OnStart();
-	}
-	
-	public void Update() {
-		OnUpdate();
-	}
-	
-	//Template Method Pattern:
-	public abstract void OnStart();	
-	public abstract void OnUpdate();		
-}
-
-
-
-
 public abstract class IState : MonoBehaviour
 {	
 	public GameObject StateGameObject;
