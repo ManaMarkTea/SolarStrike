975d5becadeef467693b6e1c4c47dab692c524ef
mtulew1 <mtulew1@uic.edu>
2014-02-17 23:45:11 -0600

    Ooops, broke stuff.

--- Assets/Scripts/Billboard.cs
+++ Assets/Scripts/Billboard.cs
@@ -15,7 +15,6 @@ public class Billboard : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-				
 		transform.LookAt(Camera.main.transform.position, -Vector3.up);
 		transform.Rotate(extraRX, extraRY, extraRZ);
 	}
